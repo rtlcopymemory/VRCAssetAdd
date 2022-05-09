94d699d79be5456434cf51e563eb7014ec0a868d
ThatOnePizza <info@protocli.me>
2022-05-09 23:03:13 +0200

    adds unstages files by mistake

---
+++ VertexIdentifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.VRCAssetAdd
+{
+    public class VertexIdentifier
+    {
+        public Vector3 Position { get; set; }
+        public VRCATriangle Triangle { get; set; }
+    }
+
+    public class VRCATriangle
+    {
+        /// TODO: These needs to be the coordinates of the verticies
+        public int a;
+        public int b;
+        public int c;
+
+        public bool Contains(int index)
+        {
+            return a == index || b == index || c == index;
+        }
+    }
+}
